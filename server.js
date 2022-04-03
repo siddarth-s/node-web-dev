79a5fcd68f55a0d93ef85542ed067f84d0d7db99
Siddarth Sathyanarayanan <siddarth@Siddarths-MacBook-Pro.local>
2022-04-03 18:11:36 -0400

    [feat]: creating HTTP controllers

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // const express = require('express');
 import express from 'express';
+import helloController from "./controllers/hello-controller.js";
+
 const app = express();
-app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+helloController(app);
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 app.listen(4000);
