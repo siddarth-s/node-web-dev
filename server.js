a1c7ac00415c611029713ed82ca3d17d73402f90
Siddarth Sathyanarayanan <siddarth@Siddarths-MacBook-Pro.local>
2022-04-03 18:44:31 -0400

    [feat]: requesting data from web server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // const express = require('express');
 import express from 'express';
 import helloController from "./controllers/hello-controller.js";
+import userController from "./controllers/user-controller.js";
 
 const app = express();
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
 helloController(app);
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
+userController(app);
 app.listen(4000);
