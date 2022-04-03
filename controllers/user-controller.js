197663142a9b02c760877035a8d7fe2cf8ccbc07
Siddarth Sathyanarayanan <siddarth@Siddarths-MacBook-Pro.local>
2022-04-03 19:25:01 -0400

    [feat]: sending query parameters to web server

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,7 +5,18 @@ const userController = (app) => {
     app.get('/api/users', findAllUsers);
 }
 
+const findUsersByType = (type) => {
+    const usersofType = users.filter(obj => obj['type'] === type);
+    return usersofType.map(x => x.username);
+}
+
 const findAllUsers = (req, res) => {
+
+    const type = req.query.type;
+    if(type){
+        res.json(findUsersByType(type));
+        return;
+    }
     res.json(users);
 }
 
