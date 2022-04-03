0c20e4d580e9200d9abcd9ad6cf0cc92a3e11904
Siddarth Sathyanarayanan <siddarth@Siddarths-MacBook-Pro.local>
2022-04-03 19:46:37 -0400

    [feat]: sending path parameters to a web server

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,11 +3,19 @@ let users = people;
 
 const userController = (app) => {
     app.get('/api/users', findAllUsers);
+    app.get('/api/users/:uid', findUserById);
+}
+
+const findUserById = (req, res) => {
+    const userId = req.params.uid;
+    const user = users.find(u => u._id === userId);
+    res.json(user);
 }
 
 const findUsersByType = (type) => {
     const usersofType = users.filter(obj => obj['type'] === type);
-    return usersofType.map(x => x.username);
+    return usersofType.map(x => x.username); //uncomment this line to display without the json
+    // return usersofType;
 }
 
 const findAllUsers = (req, res) => {
