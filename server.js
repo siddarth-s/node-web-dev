b161a5b8cc125a5d598c10262f173dec3b9995a0
Siddarth Sathyanarayanan <siddarth@Siddarths-MacBook-Pro.local>
2022-04-06 16:55:59 -0400

    [fix]: uncommented get messages

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.get('/hello', (req, res) => {res.send('Life is good!')})
+app.get('/hello', (req, res) => {res.send('Life is good!')})
 helloController(app);
-// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
 userController(app);
 tuitsController(app);
 app.listen(process.env.PORT || 4000);
