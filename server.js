37b2b529099e5ffaf49787022a5d4b06e0771639
Siddarth Sathyanarayanan <siddarth@Siddarths-MacBook-Pro.local>
2022-04-06 10:23:00 -0400

    [feat]: added cors library

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 // const express = require('express');
 import express from 'express';
+import cors from 'cors';
+
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
 helloController(app);
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')});
 userController(app);
 tuitsController(app);
 app.listen(4000);
